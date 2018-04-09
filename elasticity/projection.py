75394f1815ca8a66d5df2a0499b93c1ec6037b2a
Nicole Spillane <nicole.spillane@cmap.polytechnique.fr>
2018-04-09 11:04:36 +0200

    projection with cholesky

--- elasticity/projection.py
+++ elasticity/projection.py
@@ -29,48 +29,83 @@ class projection:
             for ivec, rbm_vec in enumerate(rbm_vecs):
                 coarse_vecs.append(da.createGlobalVec())
                 work1.set(0)
+                coarse_vecs[-1].set(0) #NICOLE: do I need this ? 
                 da.globalToLocal(rbm_vec, work2)
                 if i == mpi.COMM_WORLD.rank:
                     work1 = work2*Rlocal/Dlocal
                 da.localToGlobal(work1, coarse_vecs[-1], addv=PETSc.InsertMode.ADD_VALUES)
 
-        # orthonormalize
+        self.Delta = PETSc.Mat().create(comm=PETSc.COMM_SELF)
+        self.Delta.setType(PETSc.Mat.Type.SEQDENSE)
+        self.Delta.setSizes([len(coarse_vecs),len(coarse_vecs)])
+        self.Delta.setOption(PETSc.Mat.Option.SYMMETRIC, True)
+        self.Delta.setPreallocationDense(None)
+
+        #scale 
         coarse_Avecs = []
         for vec in coarse_vecs:
             coarse_Avecs.append(A*vec)
-
-        for i, vec in enumerate(coarse_vecs):
-            alphas = []
-            for vec_ in coarse_Avecs[:i]:
-                alphas.append(vec.dot(vec_))
-            for alpha, vec_ in zip(alphas, coarse_vecs[:i]):
-                vec.axpy(-alpha, vec_)
             vec.scale(1./np.sqrt(vec.dot(A*vec)))
-            coarse_Avecs[i] = A*vec
+            coarse_Avecs[-1] = A*vec
+        #fill coarse problem matrix
+        for i, vec in enumerate(coarse_vecs):
+            for j in range(i+1):
+                tmp = coarse_Avecs[j].dot(vec)
+                self.Delta[i, j] = tmp
+                self.Delta[j, i] = tmp
 
+        self.Delta.assemble()
         self.coarse_vecs = coarse_vecs
         self.coarse_Avecs = coarse_Avecs
         
+        self.ksp_Delta = PETSc.KSP().create(comm=PETSc.COMM_SELF)
+        self.ksp_Delta.setOperators(self.Delta)
+        #self.ksp_Delta.getType()
+        self.ksp_Delta.setType('preonly')
+        pc = self.ksp_Delta.getPC()
+        pc.setType('cholesky')
+
+#        if mpi.COMM_WORLD.rank == 3:
+#            self.Delta.view()
+
         self.work = da.createGlobalVec()
+        self.gamma = PETSc.Vec().create(comm=PETSc.COMM_SELF)
+        self.gamma.setType(PETSc.Vec.Type.SEQ)
+        self.gamma.setSizes(len(coarse_vecs))
+
 
     def apply(self, x):
+        alpha = self.gamma.duplicate()
+        for i, Avec in enumerate(self.coarse_Avecs):
+            self.gamma[i] = Avec.dot(x)
 
-        self.work.set(0)
-        for vec, Avec in zip(self.coarse_vecs, self.coarse_Avecs):
-            self.work += Avec.dot(x)*vec
+        self.ksp_Delta.solve(self.gamma, alpha)
         
-        x -= self.work
+        for i in range(len(self.coarse_vecs)):
+            x.axpy(-1.*alpha[i], self.coarse_vecs[i])
+ #       for vec, Avec in zip(self.coarse_vecs, self.coarse_Avecs):
+ #           self.work += Avec.dot(x)*vec
+ #       
 
     def xcoarse(self, rhs):
+        alpha = self.gamma.duplicate()
+        for i, vec in enumerate(self.coarse_vecs):
+            self.gamma[i] = vec.dot(rhs)
 
+        self.ksp_Delta.solve(self.gamma, alpha)
+        
         self.work.set(0)
-        for vec in self.coarse_vecs:
-            self.work += vec.dot(rhs)*vec
+        for i in range(len(self.coarse_vecs)):
+            self.work.axpy(alpha[i], self.coarse_vecs[i])
 
         return self.work.copy()
 
     def apply_transpose(self, x):
-        self.work.set(0)
-        for vec, Avec in zip(self.coarse_vecs, self.coarse_Avecs):
-            self.work += vec.dot(x)*Avec
-        x -= self.work
+        alpha = self.gamma.duplicate()
+        for i, vec in enumerate(self.coarse_vecs):
+            self.gamma[i] = vec.dot(x)
+
+        self.ksp_Delta.solve(self.gamma, alpha)
+        
+        for i in range(len(self.coarse_vecs)):
+            x.axpy(-1.*alpha[i], self.coarse_Avecs[i])
